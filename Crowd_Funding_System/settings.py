210e0254c3fe400bbcc8b3951ae2a4f58fd8dda1
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-09 04:38:12 +0200

    Add dotenv library to allow using .env file variables

--- Crowd_Funding_System/settings.py
+++ Crowd_Funding_System/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'Crowd_Funding_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql-connector-python',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
