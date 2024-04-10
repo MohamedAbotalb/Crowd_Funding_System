c55fca3d246550babaafaf5a3a2faef46179a9b8
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-10 13:50:44 +0200

    Add accounts urls

--- Crowd_Funding_System/urls.py
+++ Crowd_Funding_System/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('apps.accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
