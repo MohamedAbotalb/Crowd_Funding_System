2d93a73dcbd311a5f858ea2ffb6711e28abbec00
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-15 01:57:16 +0200

    Add project model, urls and some views functions and templetes

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Project
 
 # Create your views here.
+
+
+def project_details(request, title):
+    project = Project.get_project_by_title(title)
+    return render(request, 'projects/project_details.html', {'project': project})
+
+
+def projects_list(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', {'projects': projects})
