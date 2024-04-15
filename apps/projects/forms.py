51ab14f527dab3f6e6e4653d408eeb53eb99e593
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-15 21:40:34 +0200

    Add project creation form

---
+++ apps/projects/forms.py
@@ -0,0 +1,54 @@
+from django.utils import timezone
+from django import forms
+
+from .models import Project
+
+
+class ProjectForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Title", "class": "form-control"})
+    )
+
+    details = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Details", "class": "form-control", "rows": 5}),
+    )
+
+    total_target = forms.IntegerField(
+        widget=forms.NumberInput(attrs={"placeholder": "Total Target", "class": "form-control", "min": "0"}),
+    )
+
+    start_time = forms.DateField(
+        widget=forms.DateInput(attrs={"placeholder": "Start Date", "type": "date", "class": "form-control", "readonly": True}),
+        required=False,
+        initial=timezone.now(),
+    )
+
+    end_time = forms.DateField(
+        widget=forms.DateInput(attrs={"placeholder": "End Date", "type": "date", "class": "form-control"})
+    )
+
+    class Meta:
+        model = Project
+        fields = ('title', 'details', 'end_time', 'total_target', 'category')
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) < 3:
+            raise forms.ValidationError("Title must be at least 3 characters long.")
+        elif Project.objects.filter(title=title).exists() and self.instance.title != title:
+            raise forms.ValidationError("A project with this title already exists.")
+        return title
+
+    def clean_total_target(self):
+        total_target = self.cleaned_data.get('total_target')
+        if total_target is not None and total_target <= 0:
+            raise forms.ValidationError("Total Target must be a positive number.")
+        return total_target
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get("start_time")
+        end_time = cleaned_data.get("end_time")
+
+        if start_time and end_time and end_time <= start_time:
+            self.add_error("end_time", "End time should be greater than Start time.")
