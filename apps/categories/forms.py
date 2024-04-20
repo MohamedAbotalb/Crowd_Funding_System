626489abb727e2f1b873ee9337ba20bb7fa94aef
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-20 21:37:02 +0200

    Update category model and form

--- apps/categories/forms.py
+++ apps/categories/forms.py
@@ -6,3 +6,11 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name', 'description']
+
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Name", "class": "form-control"})
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Description", "class": "form-control", "rows": 3}),
+    )
