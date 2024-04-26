0f6c09accd094ba6d3f4940bb6dbcede50a1be77
naglaasaad2000 <naglaasaadmohamed@gmail.com>
2024-04-26 22:48:19 +0200

    project admin dashboard add,delete and show

--- apps/admin_dashboard/views.py
+++ apps/admin_dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django_countries import countries
-from apps.accounts.models import CustomUser
-from apps.projects.models import Project, Donation
+from apps.projects.models import Project,ProjectPicture,Comment, Reply, Donation, CustomUser, Rating
+import re
+from apps.projects.forms import ProjectForm
 from apps.categories.models import Category
+from django.utils import timezone
+from django.db.models import Avg, Max
 
 
 def index(request):
@@ -42,6 +46,42 @@ def show_projects(request):
     projects = Project.objects.all()
     return render(request, 'admin_dashboard/projects/project_list.html', {'projects': projects})
 
+
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            user_instance = CustomUser.objects.get(pk=request.user.pk)
+            project_pictures = request.FILES.getlist('pictures')
+            tags = form.cleaned_data.get('tags', [])
+
+            if len(project_pictures) == 0:
+                messages.error(request, f'You should choose at least 1 picture.')
+                return render(request, 'projects/create_project.html', {'form': form})
+
+            if len(tags) > 5:
+                form.add_error('tags', "Maximum 5 tags allowed.")
+            for tag in tags:
+                if not re.match(r'^[a-zA-Z0-9_]+$', tag):
+                    form.add_error('tags', "Tags can only contain letters, numbers, and underscore.")
+                    break
+            if form.errors:
+                return render(request, 'projects/create_project.html', {'form': form})
+
+            project = form.save(commit=False)
+            project.creator = user_instance
+            project.save()
+            form.save_m2m()
+
+            for pic in project_pictures:
+                ProjectPicture.objects.create(project=project, image=pic)
+
+            return redirect('show_projects')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/create_project.html', {'form': form})
+
+
 def featured_project(request, slug):
     project = get_object_or_404(Project, slug=slug)
     if request.method == 'POST':
@@ -51,3 +91,92 @@ def featured_project(request, slug):
         return JsonResponse({'success': True, 'is_featured': project.featured})
     else:
         return JsonResponse({'success': False, 'error': 'Invalid request'}, status=400)
+
+
+def show_project(request, slug):
+    # Fetch the project based on the provided slug
+    project = get_object_or_404(Project, slug=slug)
+    
+    # Fetch comments and their replies for the project
+    comments = Comment.objects.filter(project=project)
+    
+    
+    # Calculate days left until end time
+    end_datetime = project.end_time
+    now_datetime = timezone.now()
+    days_left = (end_datetime.date() - now_datetime.date()).days
+    
+    # Calculate the average rating for the project
+    average_rating = project.ratings.aggregate(Avg('value'))['value__avg']
+    if average_rating is not None:
+        project.rate = round(average_rating, 2)
+    else:
+        project.rate = None
+    project.save()
+    
+    # Check if the user has rated the project
+    user_rating = None
+    if request.user.is_authenticated:
+        current_user = request.user
+        user_rating_obj = Rating.objects.filter(user=current_user, project=project).first()
+        if user_rating_obj:
+            user_rating = user_rating_obj.value
+    
+    # Get related projects based on tags
+    related_projects = Project.objects.filter(tags__in=project.tags.all()).exclude(id=project.id).distinct()[:4]
+    for related_project in related_projects:
+        end_datetime = related_project.end_time
+        now_datetime = timezone.now()
+        days_left = (end_datetime.date() - now_datetime.date()).days
+        related_project.days_left = days_left
+    
+    # Retrieve the first and last donation made to the project
+    first_donation = Donation.objects.filter(project=project).order_by('created_at').first()
+    last_donation = Donation.objects.filter(project=project).order_by('-created_at').first()
+    
+    # Retrieve the top donation amount for the project
+    top_donation = Donation.objects.filter(project=project).aggregate(Max('amount'))['amount__max']
+    top_donation_user = CustomUser.objects.filter(donation__amount=Donation.objects.aggregate(max_amount=Max('amount'))['max_amount']).first()
+    
+    # Calculate the number of donors
+    num_donors = Donation.objects.filter(project=project).values('user').distinct().count()
+    
+    # Check if the user is the creator and current fund is less than 25% of target
+    allow_cancel = False
+    if request.user.id == project.creator.id and project.percentag < 25:
+        allow_cancel = True
+    
+    context = {
+        'project': project,
+        'comments': comments,
+        'days_left': days_left,
+        'user_rating': user_rating,
+        'average_rating': average_rating,
+        'related_projects': related_projects,
+        'first_donation': first_donation,
+        'last_donation': last_donation,
+        'top_donation': top_donation,
+        'top_donation_user': top_donation_user,
+        'num_donors': num_donors,
+        'allow_cancel' : allow_cancel, 
+    }
+    
+    return render(request, 'admin_dashboard/projects/project_show.html', context)
+
+
+def delete_project(request, slug):
+    project = get_object_or_404(Project, slug=slug)
+    if request.method == 'POST':
+        project.delete()
+        messages.success(request, 'Project deleted successfully.')
+        # Redirect to the admin dashboard index page after deletion
+        return redirect('show_projects')
+    return render(request, 'admin_dashboard/projects/project_list.html', {'project': project})
+
+def delete_comment(request, slug, id):
+    # Get the comment object
+    comment = get_object_or_404(Comment, id=id)
+    # Delete the comment
+    comment.delete()
+    # Redirect back to the project details page
+    return redirect('show_project', slug=slug)
