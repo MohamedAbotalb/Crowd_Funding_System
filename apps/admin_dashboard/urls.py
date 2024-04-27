4cd18e680974e6a4ce4ee9b81daf61da10a44102
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-04-27 18:40:27 +0300

    Update views and urls in admin dashboard

--- apps/admin_dashboard/urls.py
+++ apps/admin_dashboard/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='dashboard'),
+    path('index/', views.index, name='admin_dashboard'),
     path('users/', views.show_users, name='show_users'),
     path('users/<int:id>/delete/', views.delete_user, name='delete_user'),
     path('projects/', views.show_projects, name='show_projects'),
     path('projects/create/', views.create_project, name='create_project-admin'),
     path('projects/<slug:slug>/', views.show_project, name='show_project'),
-    # path('projects/<slug:slug>/update/', views.update_project, name='update_project'),
+    path('projects/<slug:slug>/update/', views.update_project, name='update_project'),
     path('projects/<slug:slug>/featured/', views.featured_project, name='featured_project'),
     path('projects/<slug:slug>/delete/', views.delete_project, name='delete_project'),
-    # path('projects/<slug:slug>/comments/', views.show_project_comments, name='show_project_comments'),
+    path('projects/<slug:slug>/comments/', views.show_project_comments, name='show_project_comments'),
     path('projects/<slug:slug>/comments/<int:id>/delete/', views.delete_comment, name='delete_comment'),
     path('categories/', views.show_categories, name='show_categories'),
     path('categories/create/', views.create_category, name='create_category'),
